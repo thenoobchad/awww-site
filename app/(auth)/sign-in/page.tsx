31393a33bc09d08e07af8f106f99f8cc4607bce4
thenoobchad <noobchaddy@gmail.com>
2025-04-26 11:02:32 +0100

    auth

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,18 +1,18 @@
-// "use client";
+"use client";
 
-// import { AuthForm } from "@/components/AuthForm";
-// import { signInSchema } from "@/validations";
+import { AuthForm } from "@/components/AuthForm";
+import { signInSchema } from "@/validations";
 
-// export default function SignIn() {
-// 	return (
-// 		<AuthForm
-// 			type="SIGN_IN"
-// 			schema={signInSchema}
-// 			defaultValues={{
-// 				email: "",
-// 				password: "",
-// 			}}
-// 			onSubmit={() => {}}
-// 		/>
-// 	);
-// }
+export default function SignIn() {
+	return (
+		<AuthForm
+		// type="SIGN_IN"
+		// schema={signInSchema}
+		// defaultValues={{
+		// 	email: "",
+		// 	password: "",
+		// }}
+		// onSubmit={() => {}
+		/>
+	);
+}
